229d0003c9ebf30ab5a2ea486f00469fdf14bfc2
StefanoR <rubinetti.stefano@gmail.com>
2026-03-26 11:46:15 +0100

    aggiunto Prelievo

--- src/entities/Conto.java
+++ src/entities/Conto.java
@@ -6,16 +6,30 @@ public class Conto {
 
     public String numeroConto;
     public String pin;
+    public double saldo;
 
-    public Conto(String n,String p)
+    public Conto(String n,String p,double saldoIniziale)
     {
         numeroConto=n;
         pin=p;
+        saldo=saldoIniziale;
     }
 
 
     public boolean provaAccesso(String nC, String p) {
 
         return nC.equals(numeroConto) && p.equals(pin);
+    }
+
+    public void prelievo(int valorePrelievoBancomat)
+    {
+        if(valorePrelievoBancomat>saldo)
+        {
+            Console.print("Il tuo saldo rimante è inferiore al valore che vuoi prelevare");
+            return;//return; nei metodi VOID TERMINA ESECUZIONE METODO
+        }
+
+        saldo-=valorePrelievoBancomat;
+        Console.print("Operazione conclusa con successo, saldo rimanente: "+saldo);
     }
 }
--- src/entities/ProvaConto.java
+++ src/entities/ProvaConto.java
@@ -3,12 +3,21 @@ import libreria.Console;
 
 void main()
 {
-    Conto c = new Conto("A1","12345");
+    Conto c = new Conto("A1","12345",100);
 
-    Console.print("Inserisci numero conto");
-    String n = Console.readString();
-    Console.print("Inserisci pin");
-    String p = Console.readString();
+//    Console.print("Inserisci numero conto");
+//    String n = Console.readString();
+//    Console.print("Inserisci pin");
+//    String p = Console.readString();
+//
+//    Console.print("Accesso "+(c.provaAccesso(n,p)?"consentito":"negato"));
 
-    Console.print("Accesso "+(c.provaAccesso(n,p)?"consentito":"negato"));
+    //permettere e impostare saldo a 30
+    c.prelievo(70);
+
+    //negare
+    c.prelievo(40);
+
+    //stampare 30
+    Console.print(c.saldo);
 }
