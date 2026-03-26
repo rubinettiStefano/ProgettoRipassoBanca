8f86559ddc11ef10e0e9e5fc4044a84ad6b51f35
StefanoR <rubinetti.stefano@gmail.com>
2026-03-26 16:19:12 +0100

    aggiunto buona parte main

--- src/Avvio.java
+++ src/Avvio.java
@@ -2,17 +2,17 @@ import libreria.Console;
 import entities.Conto;
 
 void main() {
-    Conto[] conti = {
-            new Conto("A1", "12345", 1500.50),
+    Conto[] conti = new Conto[]{
+            new Conto("A1", "12345", 1500.00),
             new Conto("B2", "98765", 4230.00),
-            new Conto("D3", "55443", 890.25),
+            new Conto("D3", "55443", 890.00),
             new Conto("Z4", "11223", 9450.00),
-            new Conto("T5", "67890", 120.75),
+            new Conto("T5", "67890", 120.00),
             new Conto("H6", "13579", 5600.00),
-            new Conto("A7", "24680", 3100.40),
+            new Conto("A7", "24680", 3100.00),
             new Conto("R8", "99887", 750.00),
-            new Conto("A9", "44556", 8200.15),
-            new Conto("P10", "77665", 2345.60)
+            new Conto("A9", "44556", 8200.00),
+            new Conto("P10", "77665", 2345.00)
     };
 
     Conto mio = null;
@@ -30,12 +30,80 @@ void main() {
         switch (cmd) {
             case 0 -> help();
             case 1 -> stampaConto(mio);
+            case 2 -> stampaResoconto(mio);
+            case 3 -> faiPrelievo(mio);
+            case 4 -> versaAssegno(mio);
+            case 5 -> versaBanconote(mio);
             case 99 -> mio = cambiaConto(conti, mio);
             case -1 -> Console.print("Uscita dal programma. Buon lavoro!\n");
             default -> Console.print("Comando non presente. Riprova.\n");
         }
 
     } while (cmd != -1);
+}
+
+void versaBanconote(Conto mio)
+{
+    Console.print("Quante banconote vuoi versare");
+    int nBanconote = Console.readInt();
+
+    int[] banconote = new int[nBanconote];
+    Console.print("Ora inserirai "+nBanconote +" banconote, metti:"+"\n"+
+                    "1 - 5 euro\n"+
+                    "2 - 10 euro\n"+
+                    "3 - 20 euro\n"+
+                    "4 - 50 euro\n"+
+                    "5 - 100 euro\n"+
+                    "6 - 500 euro"
+    );
+    for (int i = 0; i <banconote.length ; i++)
+    {
+        boolean ripeti=false;
+        do {
+            Console.print("Inserisci numero per banconota");
+            int n = Console.readInt();
+            switch (n)
+            {
+                case 1->banconote[i]=5;
+                case 2->banconote[i]=10;
+                case 3->banconote[i]=20;
+                case 4->banconote[i]=50;
+                case 5->banconote[i]=100;
+                case 6->banconote[i]=500;
+                default ->
+                        {
+                            Console.print("numero non valido");
+                            ripeti=true;
+                        }
+            }
+        }while (ripeti && banconote[i]==0);
+    }
+    mio.versamentoBanconote(banconote);
+}
+
+private void versaAssegno(Conto mio)
+{
+    Console.print("Quanto vuoi versare");
+    int val = Console.readInt();
+    mio.versamentoAssegno(val);
+}
+
+void faiPrelievo(Conto mio)
+{
+    Console.print("Quanto vuoi prelevare");
+    int val = Console.readInt();
+    mio.prelievo(val);
+}
+
+void stampaResoconto(Conto mio)
+{
+    if(mio.resoconto.equalsIgnoreCase(""))
+        Console.print("Non hai ancora effettuato operazioni");
+    else
+    {
+        Console.print("Ecco il resoconto delle tue operazioni");
+        Console.print(mio.resoconto);
+    }
 }
 
 void stampaConto(Conto mio)
